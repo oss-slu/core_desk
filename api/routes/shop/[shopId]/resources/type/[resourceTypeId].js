5d38db407d1fb0861af2449fdc3d40bdd4acfd6d
Jack Crane <jack@jackcrane.rocks>
2024-10-25 17:24:44 -0500

    Add CRU for resourceType

---
+++ api/routes/shop/[shopId]/resources/type/[resourceTypeId].js
@@ -0,0 +1,123 @@
+import { prisma } from "#prisma";
+import { verifyAuth } from "#verifyAuth";
+import { LogType } from "@prisma/client";
+
+export const get = [
+  verifyAuth,
+  async (req, res) => {
+    const { shopId, resourceTypeId } = req.params;
+
+    const userShop = prisma.userShop.findFirst({
+      where: {
+        userId: req.user.id,
+        shopId: shopId,
+        active: true,
+      },
+    });
+
+    if (!userShop) {
+      res.status(400).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const shouldLoadAll =
+      req.user.admin ||
+      userShop.accountType === "ADMIN" ||
+      userShop.accountType === "OPERATOR";
+
+    const resourceType = await prisma.resourceType.findFirst({
+      where: {
+        id: resourceTypeId,
+        shopId,
+        active: shouldLoadAll ? undefined : true,
+      },
+    });
+
+    if (!resourceType) {
+      return res.status(404).json({
+        message: "Resource type not found",
+      });
+    }
+
+    const resources = await prisma.resource.findMany({
+      where: {
+        shopId: shopId,
+        resourceTypeId: resourceTypeId,
+        active: shouldLoadAll ? undefined : true,
+      },
+    });
+
+    res.json({ resources, resourceType });
+  },
+];
+
+export const put = [
+  verifyAuth,
+  async (req, res) => {
+    const { shopId, resourceTypeId } = req.params;
+
+    const userShop = prisma.userShop.findFirst({
+      where: {
+        userId: req.user.id,
+        shopId: shopId,
+        active: true,
+      },
+    });
+
+    if (!userShop) {
+      res.status(400).json({
+        message: "Unauthorized",
+      });
+    }
+
+    if (
+      !req.user.admin &&
+      userShop.accountType !== "ADMIN" &&
+      userShop.accountType !== "OPERATOR"
+    ) {
+      return res.status(400).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const resourceType = await prisma.resourceType.findFirst({
+      where: {
+        id: resourceTypeId,
+        shopId,
+      },
+    });
+
+    if (!resourceType) {
+      return res.status(404).json({
+        message: "Resource type not found",
+      });
+    }
+
+    const updatedResourceType = await prisma.resourceType.update({
+      where: {
+        id: resourceTypeId,
+      },
+      data: req.body,
+      include: {
+        resources: true,
+      },
+    });
+
+    const resources = updatedResourceType.resources;
+    delete updatedResourceType.resources;
+
+    await prisma.logs.create({
+      data: {
+        userId: req.user.id,
+        shopId,
+        type: LogType.RESOURCE_TYPE_MODIFIED,
+        from: JSON.stringify(resourceType),
+        to: JSON.stringify(updatedResourceType),
+        resourceTypeId,
+      },
+    });
+
+    res.json({ resourceType: updatedResourceType, resources });
+  },
+];
---
+++ api/routes/shop/[shopId]/resources/type/index.js
@@ -0,0 +1,90 @@
+import { prisma } from "#prisma";
+import { verifyAuth } from "#verifyAuth";
+import { LogType } from "@prisma/client";
+
+export const get = [
+  verifyAuth,
+  async (req, res) => {
+    const { shopId } = req.params;
+
+    const userShop = prisma.userShop.findFirst({
+      where: {
+        userId: req.user.id,
+        shopId: shopId,
+        active: true,
+      },
+    });
+
+    if (!userShop) {
+      res.status(400).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const shouldLoadAll =
+      req.user.admin ||
+      userShop.accountType === "ADMIN" ||
+      userShop.accountType === "OPERATOR";
+
+    const resourceTypes = await prisma.resourceType.findMany({
+      where: {
+        shopId: shopId,
+        active: shouldLoadAll ? undefined : true,
+      },
+    });
+
+    res.json(resourceTypes);
+  },
+];
+
+export const post = [
+  verifyAuth,
+  async (req, res) => {
+    const { shopId } = req.params;
+
+    const userShop = prisma.userShop.findFirst({
+      where: {
+        userId: req.user.id,
+        shopId: shopId,
+        active: true,
+      },
+    });
+
+    if (!userShop) {
+      res.status(400).json({
+        message: "Unauthorized",
+      });
+    }
+
+    if (
+      !req.user.admin &&
+      userShop.accountType !== "ADMIN" &&
+      userShop.accountType !== "OPERATOR"
+    ) {
+      res.status(400).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const { title } = req.body;
+
+    const resourceType = await prisma.resourceType.create({
+      data: {
+        title,
+        shopId,
+      },
+    });
+
+    await prisma.logs.create({
+      data: {
+        type: LogType.RESOURCE_TYPE_CREATED,
+        userId: req.user.id,
+        shopId,
+        resourceTypeId: resourceType.id,
+        to: JSON.stringify(resourceType),
+      },
+    });
+
+    res.json({ resourceType });
+  },
+];
