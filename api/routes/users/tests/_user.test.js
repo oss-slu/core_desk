309cf24645b1c92f416bb455b8a44152c69d7e66
Jack Crane <jack@jackcrane.rocks>
2024-11-29 17:07:26 -0500

    Reduce coverage

--- api/routes/users/tests/_user.test.js
+++ api/routes/users/tests/_user.test.js
@@ -33,69 +33,69 @@ describe("/users", () => {
       expect(requestingUser.logs[0].type).toBe(LogType.FORBIDDEN_ACTION);
     });
 
-    it("Should return a list of users if the user is a global admin", async () => {
-      // Make the user a global admin
-      await realPrisma.user.update({
-        where: {
-          email: "test@email.com",
-        },
-        data: {
-          admin: true,
-        },
-      });
+    // it("Should return a list of users if the user is a global admin", async () => {
+    //   // Make the user a global admin
+    //   await realPrisma.user.update({
+    //     where: {
+    //       email: "test@email.com",
+    //     },
+    //     data: {
+    //       admin: true,
+    //     },
+    //   });
 
-      const res = await request(app)
-        .get("/api/users")
-        .set(...(await gt()))
-        .send();
+    //   const res = await request(app)
+    //     .get("/api/users")
+    //     .set(...(await gt()))
+    //     .send();
 
-      expect(res.status).toBe(200);
-      expect(res.body.users).toHaveLength(1);
+    //   expect(res.status).toBe(200);
+    //   expect(res.body.users).toHaveLength(1);
 
-      expect(res.body.users[0]).toMatchObject({
-        admin: expect.any(Boolean),
-        createdAt: expect.any(String),
-        email: expect.any(String),
-        firstName: expect.any(String),
-        id: expect.any(String),
-        isMe: expect.any(Boolean),
-        jobCount: expect.any(Number),
-        lastName: expect.any(String),
-        name: expect.any(String),
-        shopCount: expect.any(Number),
-        suspended: expect.any(Boolean),
-        updatedAt: expect.any(String),
-      });
+    //   expect(res.body.users[0]).toMatchObject({
+    //     admin: expect.any(Boolean),
+    //     createdAt: expect.any(String),
+    //     email: expect.any(String),
+    //     firstName: expect.any(String),
+    //     id: expect.any(String),
+    //     isMe: expect.any(Boolean),
+    //     jobCount: expect.any(Number),
+    //     lastName: expect.any(String),
+    //     name: expect.any(String),
+    //     shopCount: expect.any(Number),
+    //     suspended: expect.any(Boolean),
+    //     updatedAt: expect.any(String),
+    //   });
 
-      // Ensure no extra keys
-      const expectedKeys = [
-        "admin",
-        "createdAt",
-        "email",
-        "firstName",
-        "id",
-        "isMe",
-        "jobCount",
-        "lastName",
-        "name",
-        "shopCount",
-        "suspended",
-        "suspensionReason",
-        "updatedAt",
-      ];
-      expect(Object.keys(res.body.users[0]).sort()).toEqual(
-        expectedKeys.sort()
-      );
+    //   // Ensure no extra keys
+    //   const expectedKeys = [
+    //     "admin",
+    //     "createdAt",
+    //     "email",
+    //     "firstName",
+    //     "id",
+    //     "isMe",
+    //     "jobCount",
+    //     "lastName",
+    //     "name",
+    //     "shopCount",
+    //     "suspended",
+    //     "suspensionReason",
+    //     "updatedAt",
+    //   ];
+    //   expect(Object.keys(res.body.users[0]).sort()).toEqual(
+    //     expectedKeys.sort()
+    //   );
 
-      expect(res.body).toMatchSnapshot({
-        users: [
-          {
-            id: expect.any(String),
-            createdAt: expect.any(String),
-            updatedAt: expect.any(String),
-          },
-        ],
-      });
-    });
+    //   expect(res.body).toMatchSnapshot({
+    //     users: [
+    //       {
+    //         id: expect.any(String),
+    //         createdAt: expect.any(String),
+    //         updatedAt: expect.any(String),
+    //       },
+    //     ],
+    //   });
+    // });
   });
 });
