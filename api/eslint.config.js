1d234e9d0d02c0a45d5e04a97d5d48036f375c0c
Jack Crane <jack@jackcrane.rocks>
2024-11-29 21:30:49 -0500

    Add max len and max lines per function

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -7,4 +7,35 @@ export default [
   {
     ignores: ["**/static/tabler.js"],
   },
+  {
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "error",
+      "max-len": [
+        "error",
+        {
+          code: 80,
+          ignoreComments: true,
+          ignoreTrailingComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+        },
+      ],
+      "max-lines-per-function": [
+        "warn",
+        {
+          max: 100,
+          skipBlankLines: true,
+          skipComments: true,
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.test.js", "**/*.spec.js"], // Match test files
+    rules: {
+      "max-lines-per-function": "off", // Disable the rule for test files
+    },
+  },
 ];
