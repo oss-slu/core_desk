27d3a27d099432c4fd017334c421bf733f5b694f
Jack Crane <jack@jackcrane.rocks>
2025-03-24 14:35:04 -0500

    Fix job creation

--- api/routes/shop/[shopId]/job/index.js
+++ api/routes/shop/[shopId]/job/index.js
@@ -131,16 +131,28 @@ export const post = [
         }
       }
 
-      const job = await prisma.job.create({
-        data: {
-          title,
-          description,
-          shop: { connect: { id: shopId } },
-          user: { connect: { id: userToCreateJobAs } },
-          group: { connect: { id: billingGroupToCreateJobAs?.id } },
-          dueDate: new Date(dueDate),
-        },
-      });
+      if (billingGroupToCreateJobAs) {
+        const job = await prisma.job.create({
+          data: {
+            title,
+            description,
+            shop: { connect: { id: shopId } },
+            user: { connect: { id: userToCreateJobAs } },
+            group: { connect: { id: billingGroupToCreateJobAs?.id } },
+            dueDate: new Date(dueDate),
+          },
+        });
+      } else {
+        const job = await prisma.job.create({
+          data: {
+            title,
+            description,
+            shop: { connect: { id: shopId } },
+            user: { connect: { id: userToCreateJobAs } },
+            dueDate: new Date(dueDate),
+          },
+        });
+      }
 
       await prisma.logs.create({
         data: {
