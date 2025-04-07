50bf0ced75e872fca1091acc58a4bbad86c9afd5
ThomasMacas <tomemacas@gmail.com>
2025-04-07 12:54:34 -0500

    Back end implementation for creating a shop

--- app/src/hooks/useShops.jsx
+++ app/src/hooks/useShops.jsx
@@ -1,5 +1,72 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { authFetch } from "../util/url";
+import { Input, Util } from "tabler-react-2";
+import { useModal } from "tabler-react-2/dist/modal";
+import { ShopUserPicker } from "../components/shopUserPicker/ShopUserPicker";
+import { Button } from "tabler-react-2/dist/button";
+
+const CreateShopModalContent = ({ onSubmit }) => {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+
+  return(
+    <div>
+      <Input
+        value={name}
+        onChange={(e) => setName(e)}
+        label="Shop Name"
+        placeholder="e.g. Example Name"
+      />
+      <Input
+        value={address}
+        onChange={(e) => setAddress(e)}
+        label="Shop Address"
+        placeholder="e.g. 24 N Grand Blvd"
+      />
+      <Input
+        value={email}
+        onChange={(e) => setEmail(e)}
+        label="Shop Email"
+        placeholder="e.g. shop@slu.edu"
+      />
+      <Input
+        value={phone}
+        onChange={(e) => setPhone(e)}
+        label="Shop Phone"
+        placeholder="e.g. 123-456-7890"
+      />
+      <Input
+        value={description}
+        onChange={(e) => setDescription(e)}
+        label="Job description (optional)"
+        placeholder="e.g. Description"
+      />
+      <Util.Spacer size={1} />
+      {name.length > 1 ? (
+        <Button
+          variant = "primary"
+          onClick={() => {
+            onSubmit(
+              name,
+              address, 
+              phone,
+              email,
+              description,
+            );
+          }}
+        >
+          Submit
+        </Button>
+
+      ) : (
+        <Button disabled>Submit</Button>
+      )}
+    </div>
+  );
+}
 
 export const useShops = () => {
   const [loading, setLoading] = useState(true);
@@ -7,6 +74,34 @@ export const useShops = () => {
   const [shops, setShops] = useState([]);
   const [meta, setMeta] = useState(null);
   const [opLoading, setOpLoading] = useState(false);
+
+  const _createShop = async (
+    name,
+    address, 
+    phone,
+    email,
+    description,
+  ) => {
+    try {
+      const r = await authFetch(`api/shops`, {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          address,
+          phone,
+          email,
+          description,
+        }),
+      });
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const { modal, ModalElement } = useModal({
+      title: "Create a new Shop",
+      text: <CreateShopModalContent onSubmit={_createShop} />,
+  });
 
   const fetchShops = async (shouldSetLoading = true) => {
     try {
@@ -20,6 +115,10 @@ export const useShops = () => {
       setError(error);
       setLoading(false);
     }
+  };
+
+  const createShop = async () => {
+    modal();
   };
 
   const addUserToShop = async (userId, shopId, role) => {
@@ -103,6 +202,8 @@ export const useShops = () => {
     addUserToShop,
     removeUserFromShop,
     changeUserRole,
+    ModalElement,
+    createShop,
     opLoading,
   };
 };
--- app/src/routes/shops/index.jsx
+++ app/src/routes/shops/index.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 import { useShops } from "../../hooks/useShops";
 import { Loading } from "../../components/loading/Loading";
 import { Typography } from "tabler-react-2";
 import { ShopCard } from "../../components/shopcard/ShopCard";
 import { Page, sidenavItems } from "../../components/page/page";
+import { Button } from "tabler-react-2/dist/button";
+
 const { H1 } = Typography;
 
 export const Shops = () => {
   const { user } = useAuth();
   const { shops, loading } = useShops();
+  const {
+    ModalElement,
+    createShop,
+  } = useShops();
 
   if (loading) return <Loading />;
 
   return (
     <Page sidenavItems={sidenavItems("Shops", user.admin)}>
-      <H1>Shops</H1>
+      <Util.Row justify="between" align="center">
+        <div>
+          <H1>Shops</H1>
+        </div>
+        <Button onClick={createShop}>Create Job</Button>
+      </Util.Row>
+      <Util.Spacer size={1} />
       {shops.map((shop) => (
         <ShopCard key={shop.id} shop={shop} />
-      ))}
+      ))} 
+      {ModalElement}
     </Page>
   );
 };
