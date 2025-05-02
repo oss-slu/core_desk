597521986ce76afd7ed98f92e365a5a5680eb305
lawsj <joi.laws@slu.edu>
2025-05-02 14:29:36 -0500

    Update _verifyAuth.test.js
    -fixing lint errors

--- api/util/tests/_verifyAuth.test.js
+++ api/util/tests/_verifyAuth.test.js
@@ -1,5 +1,5 @@
 
-import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import request from "supertest";
 import express from "express";
 import jwt from "jsonwebtoken";
