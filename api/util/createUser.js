df167a09293c363bfc9240c69e2f8cbf39042d73
Jack Crane <jack@jackcrane.rocks>
2025-08-13 16:33:41 -0500

    Allow creation of new users

---
+++ api/util/createUser.js
@@ -0,0 +1,58 @@
+import prisma from "#prisma";
+import { LogType } from "@prisma/client";
+
+export const createUser = async (
+  { firstName, lastName, email },
+  logging = false
+) => {
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+    },
+  });
+
+  if (logging) {
+    console.log(
+      `Created user ${user.firstName} ${user.lastName} (${user.id}).`
+    );
+  }
+
+  const shopsToJoin = await prisma.shop.findMany({
+    where: {
+      autoJoin: true,
+    },
+  });
+
+  for (const shop of shopsToJoin) {
+    await prisma.userShop.create({
+      data: {
+        userId: user.id,
+        shopId: shop.id,
+        active: true,
+      },
+    });
+
+    await prisma.logs.create({
+      data: {
+        userId: user.id,
+        type: LogType.USER_CONNECTED_TO_SHOP,
+        shopId: shop.id,
+      },
+    });
+
+    console.log(
+      `Joined shop ${shop.name} (${shop.id}) for user ${user.firstName} ${user.lastName} (${user.id}).`
+    );
+  }
+
+  await prisma.logs.create({
+    data: {
+      userId: user.id,
+      type: LogType.USER_CREATED,
+    },
+  });
+
+  return user;
+};
