77d839262f1e1119673b2022e6a4401a9fa09dfc
Jack Crane <jack@jackcrane.rocks>
2025-08-27 10:34:41 -0500

    Fix return for the uploader

--- app/src/components/upload/uploader.jsx
+++ app/src/components/upload/uploader.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Dropzone } from "../Dropzone/Dropzone";
 
 export const UploadDropzone = ({ onUploadComplete, endpoint }) => {
-  <>
-    <Dropzone onSuccessfulUpload={onUploadComplete} endpoint={endpoint} />
-  </>;
+  return (
+    <>
+      <Dropzone onSuccessfulUpload={onUploadComplete} endpoint={endpoint} />
+    </>
+  );
 };
