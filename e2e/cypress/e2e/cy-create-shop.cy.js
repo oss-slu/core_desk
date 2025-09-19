2eaf9aac883b92511c16a7104863fc5b5c2c2f5c
Jack Crane <jack@jackcrane.rocks>
2025-09-18 22:32:31 -0500

    Add create shop

--- e2e/cypress/e2e/cy-create-shop.cy.js
+++ e2e/cypress/e2e/cy-create-shop.cy.js
@@ -16,7 +16,22 @@ describe("Create Shop", () => {
       .then((jwt) => localStorage.setItem("token", jwt))
       .visit("/");
 
-    // Wait 10 seconds
-    cy.wait(10000);
+    cy.contains("Shops").click();
+    cy.screenshot("shops-page");
+
+    cy.contains("Create Shop").click();
+    cy.screenshot("create-shop");
+
+    cy.get('input[placeholder="e.g. South Campus Shop"]').type(
+      "Test Shop Name"
+    );
+    cy.get('input[placeholder="e.g. 24 N Grand Blvd"]').type("123 Main St.");
+    cy.get('input[placeholder="e.g. shop@slu.edu"]').type("test@example.com");
+
+    cy.screenshot("create-shop-inputs");
+    cy.contains("Submit").click();
+
+    cy.timeout(1000);
+    cy.screenshot("created-shop");
   });
 });
