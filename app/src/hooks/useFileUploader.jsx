df887136e970188bf3f1355be4194d66580ba35b
Muhammudy <64837648+Muhammudy@users.noreply.github.com>
2025-10-06 16:37:09 -0500

    added a few tweaks on edge cases for file uploads (#82)
    * added a few tweaks on edge cases
    
    * added eslint comment

--- app/src/hooks/useFileUploader.jsx
+++ app/src/hooks/useFileUploader.jsx
@@ -3,14 +3,13 @@ import { authFetchWithoutContentType } from "../util/url";
 import toast from "react-hot-toast";
 import { useState } from "react";
 
-
+//eslint-disable-next-line no-unused-vars
 const uploadFiles = async (url, { arg, addOrUpdateKey, deleteAllKeys }) => {
   const { files } = arg;
 
   if (!files || files.length === 0) {
     return [];
   }
-  deleteAllKeys(); //reset the keys 
   const results = [];
 
   for (let i = 0; i < files.length; i++) { //use a for loop 
@@ -50,7 +49,7 @@ export const useProgressMap = () => { //we could have this as its own file?
   const [progress, setProgress] = useState({});
 
   // Add or update a key
-  const addOrUpdateKey = (key, value) => {
+  const addOrUpdateKey = (key, value) => { //file.name : progress
     setProgress(prev => ({
       ...prev,
       [key]: value,
@@ -100,5 +99,6 @@ export const useFileUploader = (endpoint, options) => {
     progress, //return 
     loading: isMutating,
     error,
+    deleteAllKeys
   };
 };
