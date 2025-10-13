1d2e2379c48c8d6c2e69ee3a47934f4c1d384eac
Bella Ott <bella.c.ott.04@gmail.com>
2025-10-13 10:41:58 -0500

    hid toggle for admin to keep standard for them

--- app/src/components/header/Header.jsx
+++ app/src/components/header/Header.jsx
@@ -39,7 +39,7 @@ export const Header = () => {
           )}
         </h1>
       </div>
-      {user?.id && (
+      {user?.id && !user?.admin && (
         <div style={{
           marginLeft: "auto",
           marginRight: "25px",
