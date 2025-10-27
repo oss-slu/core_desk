1d631983c46e52fa3b2bbc0b3e7233ecb0116846
Yousuf Muhammud <muhammudy@gmail.com>
2025-10-27 15:00:58 -0500

    added a quickfix, basically a retry

--- api/index.js
+++ api/index.js
@@ -371,10 +371,21 @@ if (process.env.JACK == "true") {
 }
 const PORT = process.env.PORT || 3030;
 
-if (process.env.NODE_ENV !== "test") {
-  server = app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+async function startServer() {
+  try {
+    await prisma.$connect();
+
+    if (process.env.NODE_ENV !== "test") {
+      server = app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    }
+  } catch (error) {
+    process.exit(1);
+  }
 }
+
+
+startServer();
 
 export { app, server };
