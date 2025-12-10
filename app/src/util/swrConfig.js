edddfacc6e3decefcb174723e78fe4318dcfeff6
Jack Crane <jack@jackcrane.rocks>
2025-12-10 13:14:35 -0600

    Hotfix: Revalidate on mount

---
+++ app/src/util/swrConfig.js
@@ -0,0 +1,9 @@
+export const swrConfig = {
+  revalidateIfStale: false,
+  revalidateOnFocus: false,
+  revalidateOnReconnect: false,
+  revalidateOnMount: true,
+  refreshWhenHidden: false,
+  refreshWhenOffline: false,
+  shouldRetryOnError: false,
+};
