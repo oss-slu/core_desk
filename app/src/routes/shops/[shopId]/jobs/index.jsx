aeb0d0182c955db85268fddf0130c11181f1c45f
Bella Ott <bella.c.ott.04@gmail.com>
2026-02-08 15:09:00 -0600

    linting pt 2

--- app/src/routes/shops/[shopId]/jobs/index.jsx
+++ app/src/routes/shops/[shopId]/jobs/index.jsx
@@ -1,4 +1,4 @@
-import React, { act, useState } from "react";
+import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Page } from "#page";
 import { useShop } from "../../../../hooks/useShop";
