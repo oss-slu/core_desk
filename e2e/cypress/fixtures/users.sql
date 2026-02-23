236bca2912343e30efeb659175b8aa7821c0369a
Jack Crane <jack@jackcrane.rocks>
2026-02-23 14:06:17 -0600

    admins can create shops

--- e2e/cypress/fixtures/users.sql
+++ e2e/cypress/fixtures/users.sql
@@ -1,5 +1,5 @@
 INSERT INTO "user" (id,email,"firstName","lastName",admin,suspended,simple,"createdAt","updatedAt")
-VALUES ('51882c2c-2cb3-49b6-882d-a2d9bf9ee9af','user@slu.edu','Test','User',false,false,false,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+VALUES ('51882c2c-2cb3-49b6-882d-a2d9bf9ee9af','user@coredesk-test.slu.edu','Test','User',false,false,false,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 
 INSERT INTO "user" (id,email,"firstName","lastName",admin,suspended,simple,"createdAt","updatedAt")
-VALUES ('51882c2c-23b3-49b6-882d-a2d9bf9ee9af','admin@slu.edu','Test','Admin',true,false,false,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+VALUES ('51882c2c-23b3-49b6-882d-a2d9bf9ee9af','admin@coredesk-test.slu.edu','Test','Admin',true,false,false,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
