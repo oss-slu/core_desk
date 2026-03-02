e5ce20c9fd34a4f1ca430be80abba9cc2ccd751f
Jack Crane <jack@jackcrane.rocks>
2026-03-02 12:42:24 -0600

    Upgrade prisma

--- api/util/prisma.js
+++ api/util/prisma.js
@@ -1,3 +1,8 @@
-import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../prisma/generated/client";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+export const prisma = new PrismaClient({ adapter });
 export default prisma;
