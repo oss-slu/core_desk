c27bb6949b9218b1832fe7a84c465a7cc0b9bc30
Jack Crane <jack@jackcrane.rocks>
2026-03-04 09:24:25 -0600

    Fix validation

--- api/routes/shop/[shopId]/job/[jobId]/item/[jobItemId]/index.js
+++ api/routes/shop/[shopId]/job/[jobId]/item/[jobItemId]/index.js
@@ -3,18 +3,20 @@ import { prisma } from "#prisma";
 import { verifyAuth } from "#verifyAuth";
 import { z } from "zod";
 
-const jobItemUpdateSchema = z.object({
+export const jobItemUpdateSchema = z.object({
   resourceTypeId: z.string().nullable().optional(),
   resourceId: z.string().nullable().optional(),
   materialId: z.string().nullable().optional(),
   secondaryMaterialId: z.string().nullable().optional(),
-  timeQty: z.number().optional(),
-  processingTimeQty: z.number().optional(),
-  unitQty: z.number().optional(),
-  materialQty: z.number().optional(),
-  secondaryMaterialQty: z.number().optional(),
-  rawValue: z.number().optional(),
-  qty: z.number().optional(),
+
+  timeQty: z.number().optional().default(0),
+  processingTimeQty: z.number().nullable().optional().default(0),
+  unitQty: z.number().nullable().optional().default(0),
+  materialQty: z.number().nullable().optional().default(0),
+  secondaryMaterialQty: z.number().nullable().optional().default(0),
+  rawValue: z.number().nullable().optional().default(0),
+  qty: z.number().optional().default(0),
+
   approved: z.boolean().nullable().optional(),
   status: z.string().optional(),
 });
@@ -93,149 +95,155 @@ export const get = [
 export const put = [
   verifyAuth,
   async (req, res) => {
-    const { shopId, jobId, jobItemId } = req.params;
-    const userId = req.user.id;
+    try {
+      const { shopId, jobId, jobItemId } = req.params;
+      const userId = req.user.id;
 
-    const userShop = await prisma.userShop.findFirst({
-      where: {
-        userId,
-        shopId,
-        active: true,
-      },
-    });
-
-    if (!userShop) {
-      return res.status(400).json({ error: "User shop not found" });
-    }
-
-    let firstJob = await prisma.job.findFirst({
-      where: {
-        id: jobId,
-        shopId,
-      },
-    });
-
-    let job;
-    if (
-      userShop.accountType === "CUSTOMER" &&
-      !req.user.admin &&
-      !firstJob.groupId
-    ) {
-      job = await prisma.job.findFirst({
+      const userShop = await prisma.userShop.findFirst({
         where: {
-          id: jobId,
-          shopId,
           userId,
+          shopId,
+          active: true,
         },
       });
-    } else {
-      job = await prisma.job.findFirst({
+
+      if (!userShop) {
+        return res.status(400).json({ error: "User shop not found" });
+      }
+
+      let firstJob = await prisma.job.findFirst({
         where: {
           id: jobId,
           shopId,
         },
       });
-    }
 
-    if (!job) {
-      return res.status(404).json({ error: "Not found" });
-    }
+      let job;
+      if (
+        userShop.accountType === "CUSTOMER" &&
+        !req.user.admin &&
+        !firstJob.groupId
+      ) {
+        job = await prisma.job.findFirst({
+          where: {
+            id: jobId,
+            shopId,
+            userId,
+          },
+        });
+      } else {
+        job = await prisma.job.findFirst({
+          where: {
+            id: jobId,
+            shopId,
+          },
+        });
+      }
 
-    const jobItem = await prisma.jobItem.findFirst({
-      where: {
-        id: jobItemId,
-        jobId,
-        active: true,
-      },
-    });
+      if (!job) {
+        return res.status(404).json({ error: "Not found" });
+      }
 
-    if (!jobItem) {
-      return res.status(404).json({ error: "Not found" });
-    }
-
-    if (
-      (req.body.data.resourceTypeId || req.body.data.resourceTypeId === null) &&
-      req.body.data.resourceTypeId !== jobItem.resourceTypeId
-    ) {
-      req.body.data.materialId = null;
-      req.body.data.secondaryMaterialId = null;
-      req.body.data.resourceId = null;
-    }
-
-    delete req.body.data.resource;
-    delete req.body.data.resourceType;
-    delete req.body.data.material;
-    delete req.body.data.secondaryMaterial;
-
-    console.log(req.body.data);
-
-    const validationResult = jobItemUpdateSchema.safeParse(req.body.data);
-    if (!validationResult.success) {
-      return res.status(400).json({
-        error: "Invalid data",
-        issues: validationResult.error.format(),
+      const jobItem = await prisma.jobItem.findFirst({
+        where: {
+          id: jobItemId,
+          jobId,
+          active: true,
+        },
       });
+
+      if (!jobItem) {
+        return res.status(404).json({ error: "Not found" });
+      }
+
+      if (
+        (req.body.data.resourceTypeId ||
+          req.body.data.resourceTypeId === null) &&
+        req.body.data.resourceTypeId !== jobItem.resourceTypeId
+      ) {
+        req.body.data.materialId = null;
+        req.body.data.secondaryMaterialId = null;
+        req.body.data.resourceId = null;
+      }
+
+      delete req.body.data.resource;
+      delete req.body.data.resourceType;
+      delete req.body.data.material;
+      delete req.body.data.secondaryMaterial;
+
+      console.log(req.body.data);
+
+      const validationResult = jobItemUpdateSchema.safeParse(req.body.data);
+      if (!validationResult.success) {
+        return res.status(400).json({
+          error: "Invalid data",
+          issues: validationResult.error.format(),
+        });
+      }
+
+      const updatedItem = await prisma.jobItem.update({
+        where: {
+          id: jobItemId,
+          active: true,
+        },
+        data: req.body.data,
+        include: {
+          resource: {
+            select: {
+              id: true,
+              title: true,
+              costingPublic: true,
+              costPerProcessingTime: true,
+              costPerTime: true,
+              costPerUnit: true,
+            },
+          },
+          material: {
+            select: {
+              costPerUnit: true,
+              unitDescriptor: true,
+            },
+          },
+          secondaryMaterial: {
+            select: {
+              costPerUnit: true,
+              unitDescriptor: true,
+            },
+          },
+          resourceType: {
+            select: {
+              id: true,
+              title: true,
+              costingMode: true,
+            },
+          },
+          file: true,
+          fileThumbnail: true,
+        },
+      });
+
+      const updatedItemToLog = JSON.parse(JSON.stringify(updatedItem));
+      delete updatedItemToLog.resource;
+      delete updatedItemToLog.material;
+      //delete updatedItemToLog.secondaryMaterial;  MIGHT NEED TO UPDATE LOGS TABLE...
+
+      await prisma.logs.create({
+        data: {
+          userId,
+          shopId,
+          jobId,
+          jobItemId,
+          type: LogType.JOB_ITEM_MODIFIED,
+          from: JSON.stringify(jobItem),
+          to: JSON.stringify(updatedItemToLog),
+        },
+      });
+
+      return res.json({ item: updatedItem });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Internal server error" });
     }
-
-    const updatedItem = await prisma.jobItem.update({
-      where: {
-        id: jobItemId,
-        active: true,
-      },
-      data: req.body.data,
-      include: {
-        resource: {
-          select: {
-            id: true,
-            title: true,
-            costingPublic: true,
-            costPerProcessingTime: true,
-            costPerTime: true,
-            costPerUnit: true,
-          },
-        },
-        material: {
-          select: {
-            costPerUnit: true,
-            unitDescriptor: true,
-          },
-        },
-        secondaryMaterial: {
-          select: {
-            costPerUnit: true,
-            unitDescriptor: true,
-          },
-        },
-        resourceType: {
-          select: {
-            id: true,
-            title: true,
-            costingMode: true,
-          },
-        },
-        file: true,
-        fileThumbnail: true,
-      },
-    });
-
-    const updatedItemToLog = JSON.parse(JSON.stringify(updatedItem));
-    delete updatedItemToLog.resource;
-    delete updatedItemToLog.material;
-    //delete updatedItemToLog.secondaryMaterial;  MIGHT NEED TO UPDATE LOGS TABLE...
-
-    await prisma.logs.create({
-      data: {
-        userId,
-        shopId,
-        jobId,
-        jobItemId,
-        type: LogType.JOB_ITEM_MODIFIED,
-        from: JSON.stringify(jobItem),
-        to: JSON.stringify(updatedItemToLog),
-      },
-    });
-
-    return res.json({ item: updatedItem });
   },
 ];
 
