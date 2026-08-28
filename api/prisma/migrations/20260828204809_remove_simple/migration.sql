ac557d5074c99a7e3a6d972c7fea24e832f08e2f
Bella Ott <bella.c.ott.04@gmail.com>
2026-08-28 15:48:24 -0500

    prisma migration

---
+++ api/prisma/migrations/20260828204809_remove_simple/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `simple` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "user" DROP COLUMN "simple";
